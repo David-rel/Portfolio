cc209d53093f80b0d3c2225fe3f948bbe4c35626
David Fales <davidfalesct@gmail.com>
2021-12-19 22:40:46 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,95 @@
+function AboutBtnClicked(){
+    document.location.href="AboutMe.html";
+}
+
+function HomeBtnClicked(){
+    document.location.href="Index.html";
+}
+
+function ProjectsBtnClicked(){
+    document.location.href="Projects.html";
+}
+
+function SkillsBtnClicked(){
+    document.location.href="Skills.html";
+}
+
+function GitHubBtnClicked(){
+    document.location.href="https://github.com/David-rel";
+}
+
+function AcademicsBtnClicked(){
+    document.location.href="Academics.html";
+}
+
+function ContactBtnClicked(){
+    document.location.href="Contact.html";
+}
+
+function OutlookBtnClicked(){
+    document.location.href="https://outlook.office365.com/mail/";
+}
+function GMAILBtnClicked(){
+    document.location.href="https://mail.google.com/mail/u/0/#inbox";
+}
+function FacebookBtnClicked(){
+    document.location.href="https://www.facebook.com/";
+}
+function InstagramBtnClicked(){
+    document.location.href="https://www.instagram.com/sem/campaign/emailsignup/?campaign_id=13530334509&extra_1=s|c|547348909654|e|instagram%20%27|&placement=&creative=547348909654&keyword=instagram%20%27&partner_id=googlesem&extra_2=campaignid%3D13530334509%26adgroupid%3D126262421974%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-1321618851291%26loc_physical_ms%3D9028744%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIg_CTydbn9AIVivOzCh1uLQrEEAAYASAAEgJwY_D_BwE";
+}
+function TwitterBtnClicked(){
+    document.location.href="https://twitter.com/?lang=en";
+}
+
+function GitHubProjectBthClicked(choice){
+
+    if(choice == 1){
+        document.location.href="https://github.com/David-rel/Oregon-Road";
+    }
+
+    if(choice == 2){
+        document.location.href="https://github.com/David-rel/Snake";
+
+    }
+
+    if(choice == 3){
+        document.location.href="https://github.com/David-rel/Pong";
+
+    }
+
+    if(choice == 4){
+        document.location.href="https://github.com/David-rel/Instant-Messanger";
+
+    }
+
+    if(choice == 5){
+        document.location.href="https://github.com/David-rel/Portfolio";
+
+    }
+}
+
+function LearnMoreBtnClicked(choice){
+    if(choice == 1){
+        document.location.href="https://regisjesuit.myschoolapp.com/app/student#resourceboarddetail/19838";
+    }
+
+    if(choice == 2){
+        document.location.href="https://regisjesuit.myschoolapp.com/app/student#resourceboarddetail/19838";
+
+    }
+
+    if(choice == 3){
+        document.location.href="https://www.regisjesuit.com/team-detail?fromId=248910&Team=259550&SeasonLabel=2021%20-%202022&siteId=1293";
+
+    }
+
+    if(choice == 4){
+        document.location.href="https://sourcesofstrength.org/";
+
+    }
+}
+
+function RobotGitHubBthClicked(){
+    document.location.href="https://github.com/orgs/RegisJesuitRobotics/teams/2022-programmers";
+}
